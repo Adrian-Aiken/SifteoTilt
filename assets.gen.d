351233c1de83f5e42b3ceb9bb884f6ea52102f52
Adrian Aiken <goatmancer@gmail.com>
2013-01-20 00:27:30 -0500

    Initial Commit

---
+++ assets.gen.d
@@ -0,0 +1,54 @@
+assets.gen.o: assets.gen.cpp F:\Development\Sifteo/include\sifteo/asset.h \
+  F:\Development\Sifteo/include\sifteo/asset/group.h \
+  F:\Development\Sifteo/include\sifteo/abi.h \
+  F:\Development\Sifteo/include\sifteo/abi/syscall.h \
+  F:\Development\Sifteo/include\sifteo/abi/types.h \
+  F:\Development\Sifteo/include\sifteo/abi/audio.h \
+  F:\Development\Sifteo/include\sifteo/abi/events.h \
+  F:\Development\Sifteo/include\sifteo/abi/vram.h \
+  F:\Development\Sifteo/include\sifteo/abi/asset.h \
+  F:\Development\Sifteo/include\sifteo/abi/elf.h \
+  F:\Development\Sifteo/include\sifteo/limits.h \
+  F:\Development\Sifteo/include\sifteo/macros.h \
+  F:\Development\Sifteo/include\sifteo/math.h \
+  F:\Development\Sifteo/include\sifteo/asset/loader.h \
+  F:\Development\Sifteo/include\sifteo/array.h \
+  F:\Development\Sifteo/include\sifteo/memory.h \
+  F:\Development\Sifteo/include\sifteo/asset/audio.h \
+  F:\Development\Sifteo/include\sifteo/asset/image.h
+
+F:\Development\Sifteo/include\sifteo/asset.h:
+
+F:\Development\Sifteo/include\sifteo/asset/group.h:
+
+F:\Development\Sifteo/include\sifteo/abi.h:
+
+F:\Development\Sifteo/include\sifteo/abi/syscall.h:
+
+F:\Development\Sifteo/include\sifteo/abi/types.h:
+
+F:\Development\Sifteo/include\sifteo/abi/audio.h:
+
+F:\Development\Sifteo/include\sifteo/abi/events.h:
+
+F:\Development\Sifteo/include\sifteo/abi/vram.h:
+
+F:\Development\Sifteo/include\sifteo/abi/asset.h:
+
+F:\Development\Sifteo/include\sifteo/abi/elf.h:
+
+F:\Development\Sifteo/include\sifteo/limits.h:
+
+F:\Development\Sifteo/include\sifteo/macros.h:
+
+F:\Development\Sifteo/include\sifteo/math.h:
+
+F:\Development\Sifteo/include\sifteo/asset/loader.h:
+
+F:\Development\Sifteo/include\sifteo/array.h:
+
+F:\Development\Sifteo/include\sifteo/memory.h:
+
+F:\Development\Sifteo/include\sifteo/asset/audio.h:
+
+F:\Development\Sifteo/include\sifteo/asset/image.h:
---
+++ main.d
@@ -0,0 +1,113 @@
+main.o: main.cpp F:\Development\Sifteo/include\sifteo.h \
+  F:\Development\Sifteo/include\sifteo/abi.h \
+  F:\Development\Sifteo/include\sifteo/abi/syscall.h \
+  F:\Development\Sifteo/include\sifteo/abi/types.h \
+  F:\Development\Sifteo/include\sifteo/abi/audio.h \
+  F:\Development\Sifteo/include\sifteo/abi/events.h \
+  F:\Development\Sifteo/include\sifteo/abi/vram.h \
+  F:\Development\Sifteo/include\sifteo/abi/asset.h \
+  F:\Development\Sifteo/include\sifteo/abi/elf.h \
+  F:\Development\Sifteo/include\sifteo/array.h \
+  F:\Development\Sifteo/include\sifteo/memory.h \
+  F:\Development\Sifteo/include\sifteo/macros.h \
+  F:\Development\Sifteo/include\sifteo/asset.h \
+  F:\Development\Sifteo/include\sifteo/asset/group.h \
+  F:\Development\Sifteo/include\sifteo/limits.h \
+  F:\Development\Sifteo/include\sifteo/math.h \
+  F:\Development\Sifteo/include\sifteo/asset/loader.h \
+  F:\Development\Sifteo/include\sifteo/asset/audio.h \
+  F:\Development\Sifteo/include\sifteo/asset/image.h \
+  F:\Development\Sifteo/include\sifteo/audio.h \
+  F:\Development\Sifteo/include\sifteo/cube.h \
+  F:\Development\Sifteo/include\sifteo/event.h \
+  F:\Development\Sifteo/include\sifteo/filesystem.h \
+  F:\Development\Sifteo/include\sifteo/metadata.h \
+  F:\Development\Sifteo/include\sifteo/motion.h \
+  F:\Development\Sifteo/include\sifteo/string.h \
+  F:\Development\Sifteo/include\sifteo/system.h \
+  F:\Development\Sifteo/include\sifteo/time.h \
+  F:\Development\Sifteo/include\sifteo/video.h \
+  F:\Development\Sifteo/include\sifteo/video/color.h \
+  F:\Development\Sifteo/include\sifteo/video/sprite.h \
+  F:\Development\Sifteo/include\sifteo/video/framebuffer.h \
+  F:\Development\Sifteo/include\sifteo/video/bg0rom.h \
+  F:\Development\Sifteo/include\sifteo/video/bg0.h \
+  F:\Development\Sifteo/include\sifteo/video/bg1.h \
+  F:\Development\Sifteo/include\sifteo/video/bg2.h \
+  F:\Development\Sifteo/include\sifteo/video/tilebuffer.h assets.gen.h
+
+F:\Development\Sifteo/include\sifteo.h:
+
+F:\Development\Sifteo/include\sifteo/abi.h:
+
+F:\Development\Sifteo/include\sifteo/abi/syscall.h:
+
+F:\Development\Sifteo/include\sifteo/abi/types.h:
+
+F:\Development\Sifteo/include\sifteo/abi/audio.h:
+
+F:\Development\Sifteo/include\sifteo/abi/events.h:
+
+F:\Development\Sifteo/include\sifteo/abi/vram.h:
+
+F:\Development\Sifteo/include\sifteo/abi/asset.h:
+
+F:\Development\Sifteo/include\sifteo/abi/elf.h:
+
+F:\Development\Sifteo/include\sifteo/array.h:
+
+F:\Development\Sifteo/include\sifteo/memory.h:
+
+F:\Development\Sifteo/include\sifteo/macros.h:
+
+F:\Development\Sifteo/include\sifteo/asset.h:
+
+F:\Development\Sifteo/include\sifteo/asset/group.h:
+
+F:\Development\Sifteo/include\sifteo/limits.h:
+
+F:\Development\Sifteo/include\sifteo/math.h:
+
+F:\Development\Sifteo/include\sifteo/asset/loader.h:
+
+F:\Development\Sifteo/include\sifteo/asset/audio.h:
+
+F:\Development\Sifteo/include\sifteo/asset/image.h:
+
+F:\Development\Sifteo/include\sifteo/audio.h:
+
+F:\Development\Sifteo/include\sifteo/cube.h:
+
+F:\Development\Sifteo/include\sifteo/event.h:
+
+F:\Development\Sifteo/include\sifteo/filesystem.h:
+
+F:\Development\Sifteo/include\sifteo/metadata.h:
+
+F:\Development\Sifteo/include\sifteo/motion.h:
+
+F:\Development\Sifteo/include\sifteo/string.h:
+
+F:\Development\Sifteo/include\sifteo/system.h:
+
+F:\Development\Sifteo/include\sifteo/time.h:
+
+F:\Development\Sifteo/include\sifteo/video.h:
+
+F:\Development\Sifteo/include\sifteo/video/color.h:
+
+F:\Development\Sifteo/include\sifteo/video/sprite.h:
+
+F:\Development\Sifteo/include\sifteo/video/framebuffer.h:
+
+F:\Development\Sifteo/include\sifteo/video/bg0rom.h:
+
+F:\Development\Sifteo/include\sifteo/video/bg0.h:
+
+F:\Development\Sifteo/include\sifteo/video/bg1.h:
+
+F:\Development\Sifteo/include\sifteo/video/bg2.h:
+
+F:\Development\Sifteo/include\sifteo/video/tilebuffer.h:
+
+assets.gen.h:
